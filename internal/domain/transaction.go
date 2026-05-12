f247483ed493abb546b53e147c1e3a0b8a5385b3
willy <medicalawn@gmail.com>
2026-05-12 10:33:28 +0700

    feat: add riderID parameter to GetAdminStockReport in TransactionRepository interface

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -56,8 +56,9 @@ type AdminStockResponse struct {
 type TransactionRepository interface {
 	CreateSale(ctx context.Context, riderID int, creatorName string, req SaleRequest) error
 	InitiateStock(ctx context.Context, adminID string, adminName string, req StockInitiationRequest) error
-	GetAdminStockReport(ctx context.Context, riderName string, dateStart string, dateEnd string) ([]RiderStockSummary, error)
+	GetAdminStockReport(ctx context.Context, riderID string, riderName string, dateStart string, dateEnd string) ([]RiderStockSummary, error)
 }
+
 
 
 
